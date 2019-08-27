89e958583c53a8747c241a060a7ff09e5b33a6ac
Shah Muhammad <shahmuhammad11027@gmail.com>
2019-08-27 17:07:04 -0600

    users page working without photos

--- app/User.php
+++ app/User.php
@@ -3,11 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     //
 
-    protected $fillable = ['name', 'email'];
+   protected $fillable = ['name', 'email', 'password', 'role_id', 'photo_id', 'is_active'];
+
+
+    protected $hidden = ['password', 'remember_token'];
+
+
+
+
+     public function role(){
+
+        return $this->belongsTo('App\Role', 'role_id');
+
+     }
+
+
+
+
+
+
 
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,27 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .styles([
+       'libs/blog-post.css',
+       'libs/bootstrap.css',
+       'libs/font-awesome.css',
+       'libs/metisMenu.css',
+       'libs/sb-admin-2.css'
+      
+   	], './public/css/libs.css')
+    .scripts([
+    
+         'libs/jquery.js',
+         'libs/bootstrap.js',  
+         'libs/metisMenu.js',
+         'libs/sb-admin-2.js',
+         'libs/scripts.js'
+ 
+], './public/js/libs.js'); 
+
+     
+
+
+
+
